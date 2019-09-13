e2b65481f58f7f092ac2a3ce4727fbaaad673df5
prkrsign <prkrsign@yahoo.co.jp>
2019-09-13 20:05:57 +0900

    setting end

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tests#index'
   get '/show', to: 'tests#show'
+  get 'products/show', to: 'products#show'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
