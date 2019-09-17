275486d9e84add1c2ad4e4e41412d91d880106d1
prkrsign <prkrsign@yahoo.co.jp>
2019-09-17 09:55:26 +0900

    add enum   and gitignore DS-Store

---
+++ db/migrate/20190916121136_tests.rb
@@ -0,0 +1,4 @@
+class Tests < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_120854) do
+ActiveRecord::Schema.define(version: 2019_09_16_122148) do
+
+  create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "prefecture", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
