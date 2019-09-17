d967fd9e2a90c6441f9533d9f9baafcd4b149120
prkrsign <prkrsign@yahoo.co.jp>
2019-09-17 20:41:23 +0900

    Preparation-JavaScript

--- Gemfile
+++ Gemfile
@@ -83,3 +83,4 @@ gem 'erb2haml'
 gem 'devise'
 gem 'kaminari'
 gem "font-awesome-rails"
+gem 'jquery-rails'
---
+++ app/assets/javascripts/exhibit.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('#category').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+    })
+  })
