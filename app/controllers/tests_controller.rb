aa1f94769d046d84b567c0067dd1bbfc7b244351
prkrsign <prkrsign@yahoo.co.jp>
2019-09-19 13:02:59 +0900

    routing user-signup-view

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,7 +3,31 @@ class TestsController < ApplicationController
         
     end
 
-    def show
-        
+    def signup
+
+    end
+
+
+    def signup1
+
+    end
+
+    def signup2
+
+    end
+    def signup3
+
+    end
+
+    def signup4
+
+    end
+
+    def signup5
+
+    end
+
+    def phone_input
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   get 'goods/new'
   get 'goods/search'
-  resources :tests
   resources :goods
   devise_for :users
   root to: 'tests#index'
-  get '/show', to: 'tests#show'
   get 'products/show', to: 'products#show'
   get 'purchase/show', to: 'purchase#show'
+
+  # スプリントレビュー用
+  get 'tests/signup', to: 'tests#signup'
+  get 'tests/signup1', to: 'tests#signup1'
+  get 'tests/signup2', to: 'tests#signup2'
+  get 'tests/signup3', to: 'tests#signup3'
+  get 'tests/signup4', to: 'tests#signup4'
+  get 'tests/signup5', to: 'tests#signup5'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
