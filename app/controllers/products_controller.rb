7b773310adc694f52b63746383bfa38a8e31fea7
shimapokemon <p_nuts.mc@icloud.com>
2019-09-20 13:55:39 +0900

    puchased_model
    商品登録用のダミーをコメントアウト

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,13 +13,15 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Good.create(goods_name: product_params[:goods_name], goods_discription: product_params[:goods_discription], price: product_params[:price]user_id: current_user.id)
 
+    # 以下のコメントアウトはちょっと商品登録用のダミー用なのでとっといてください。シマモト
+    # Good.create(goods_name: product_params[:goods_name], goods_discription: product_params[:goods_discription], price: product_params[:price]user_id: current_user.id)
     # product = Product.create(product_params)
+     # @goods = Goods.create(goods_name: "i",goods_description: "i")
+    # Goods.create(goods_name: params[:goods_name], price: params[:price], goods_description: params[:goods_description])
     product.save!
 
-    # @goods = Goods.create(goods_name: "i",goods_description: "i")
-    # Goods.create(goods_name: params[:goods_name], price: params[:price], goods_description: params[:goods_description])@
+   
   end
 
   private
