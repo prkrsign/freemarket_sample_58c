8726200388a1746630352211ffa82a221173dd15
prkrsign <prkrsign@yahoo.co.jp>
2019-09-23 17:38:50 +0900

    fix synbolic-link-setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,10 +33,25 @@ set :default_env, {
   aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
 }
 
+# master.key用のシンボリックリンクを追加
+set :linked_files, %w{ config/master.key }
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
+    task :restart do
+      invoke 'unicorn:restart'
+    end
+
+    desc 'upload master.key'
+    task :upload do
+      on roles(:app) do |host|
+        if test "[ ! -d #{shared_path}/config ]"
+          execute "mkdir -p #{shared_path}/config"
+        end
+        upload!('config/master.key', "#{shared_path}/config/master.key")
+      end
+    end
+    before :starting, 'deploy:upload'
+    after :finishing, 'deploy:cleanup'
   end
-end
