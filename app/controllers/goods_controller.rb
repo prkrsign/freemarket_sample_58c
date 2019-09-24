71b6b030fbaccb3069d5524118f1667bbabf7039
Shuhei Maruyama <shuheimaruyama@ShuheinoMacBook-Pro.local>
2019-09-24 12:40:47 +0900

    modify_nest
    ーネストの修正
    ーインデントの微調整を行なった

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -2,7 +2,8 @@ class GoodsController < ApplicationController
 
   # トップページの商品一覧表示
   def index
-    @image = Image.find(3)
+    @image = Image.find(1)
+    @good = Good.find(1)
   end
   
   def new
