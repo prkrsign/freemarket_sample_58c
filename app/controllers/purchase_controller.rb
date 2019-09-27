a059e23e84557934402521ab0f0c835b12645f88
prkrsign <prkrsign@yahoo.co.jp>
2019-09-27 10:21:56 +0900

    add comment out

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -9,13 +9,11 @@ class PurchaseController < ApplicationController
         # @image = Image.find(17)
         # @good = Good.find(1)
         @good = Good.find(params[:id])
-        # binding.pry
     end
 
     # payjpの支払い記述(神山)
     def pay
         @good = Good.find(params[:id])
-        binding.pry
         Payjp.api_key = 'sk_test_2ef78e8e70f19787263e2df5'
         charge = Payjp::Charge.create(
         :amount => @good.price,
