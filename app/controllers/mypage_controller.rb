105826172f870fb0ff03c1ca420c40d3821f6f98
prkrsign <prkrsign@yahoo.co.jp>
2019-09-27 17:55:15 +0900

    delete test controller and routing mypage_path

---
+++ app/controllers/mypage_controller.rb
@@ -0,0 +1,25 @@
+class MypageController < ApplicationController
+    def mypage
+
+    end
+
+    def put_up_goods_on_sale
+
+    end
+
+    def purchased_on_deal
+
+    end
+
+    def edit_profile
+
+    end
+
+    def credit_enter
+
+    end
+
+    def make_sure_user_info
+        
+    end
+end
