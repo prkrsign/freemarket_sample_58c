c05394c25fe1d323e5b42e207ba850e3e8608068
shimapokemon <p_nuts.mc@icloud.com>
2019-09-28 14:00:40 +0900

    add bread crumbs
    パンくず作成
    マイページ
    プロフィール
    出品中
    支払い方法
    カード番号入力
    本人情報の登録
    ログアウト

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,15 +1,96 @@
+# 〜〜〜〜〜パンくず親〜〜〜〜〜
 crumb :root do
-  link "Home", root_path
+  link "メルカリ", root_path, class:'bread-home-btn'
 end
 
-crump :users do 
-  link 'ユーザー一覧', users_path
+# 〜〜〜〜〜パンくず子供エリア〜〜〜〜〜
+
+# マイページ
+crumb :mypage do
+  link "マイページ", mypage_path
 end
 
-crumb :user do |user|
-  link "@#{user.nickname}", user_path(user)
-  parent :users
+# ログアウト
+crumb :logout do 
+  link "ログアウト", mypage_logout_path
+  parent :mypage
 end
+
+# プロフィール
+crumb :profile do 
+  link "プロフィール", mypage_edit_profile_path
+  parent :mypage
+end
+
+
+# 出品した商品-出品中
+crumb :put_up_goods_on_sale do 
+  link "出品した商品-出品中", mypage_put_up_goods_on_sale_path
+  parent :mypage
+end
+
+# 購入した商品-取引中
+crumb :purchased_on_deal do 
+  link "購入した商品-取引中",  mypage_purchased_on_deal_path
+  parent :mypage
+end
+
+# 支払い方法
+crumb :credit_enter do 
+  link "支払い方法",    mypage_credit_enter_path
+  parent :mypage
+end
+
+# 本人情報の登録
+crumb :make_sure_user do 
+  link "本人情報の登録", mypage_make_sure_user_info_path
+  parent :mypage
+end
+
+# 〜〜〜〜〜パンくず孫エリア〜〜〜〜〜
+
+# クレジットカード情報入力
+crumb :credit_register do 
+  link "クレジットカード情報入力", mypage_credit_register_path 
+  parent :credit_enter
+end
+
+# 〜〜〜〜〜以下：パンくずの考え方と作り方 9/28 YS〜〜〜〜〜
+
+# カテゴリー＞メンズ＞トップス＞ライダースと表示させてみる場合には
+# 以下：親パン カテゴリー
+# crumb :category do
+#   link "カテゴリー＾", root_path, 
+# end
+
+# 以下：子供パン メンズ
+# crumb :mens do 
+#   link "メンズ", mens_path # ←ここのパスはPrefixの値を入れてあげよう ターミナルでrake routesで確認ができる
+# end
+
+# 以下：孫パン トップス
+# crumb :tops do 
+#   link "トップス", tops_path
+#   parent :mens # ←ここで初めてparentを使いその上の階層（ここで言うmens）を指定してあげる。
+# end
+
+# 以下：ひ孫パン ライダース
+# crumb :riders do 
+#   link "ライダース", riders_path
+#   parent :tops # ←これ以降はどんどん上の階層に紐づかせていく事でどんどん数珠繋ぎになっていく。
+# end
+
+# あとは反映させたいページに合わせて入れてあげよう、
+# 例えばライダースのカテゴリーが出てくるビューページ(htmlかhaml)に表示させたい箇所に入れてあげればOK
+
+# - breadcrumb :riders ←これが呼び出す為のメソッド名になる。
+# = breadcrumbs separator: "&rsaquo; ", class: "breadcrumbs-list"
+
+
+# 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+
+
+
 # crumb :projects do
 #   link "Projects", projects_path
 # end
