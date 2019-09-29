a8c5b1ff407fc170b7afb55e3adca2f99a83d261
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-09-29 14:05:02 +0900

    commentout

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -15,7 +15,7 @@ class CardsController < ApplicationController
       customer = Payjp::Customer.create(
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
-      ) 
+      )   #お客様情報取得。あとでjsで消すけど
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "complete"
