45979f0a397f981d39a9b6fe8829d17ec2872a6a
Shuhei Maruyama <shuheimaruyama@ShuheinoMacBook-Pro.local>
2019-10-01 16:44:32 +0900

    change birthday to birthdate
    カラム名変更したため

--- db/schema.rb
+++ db/schema.rb
@@ -98,4 +98,27 @@ ActiveRecord::Schema.define(version: 2019_09_28_115323) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username", null: false
+    t.text "user_description"
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_in_katakana", null: false
+    t.string "first_name_in_katakana", null: false
+    t.integer "birth_year", null: false
+    t.integer "birth_month", null: false
+    t.integer "birth_date", null: false
+    t.string "phone_number", null: false
+    t.bigint "address_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
