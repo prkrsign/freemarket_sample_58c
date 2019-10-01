1660ea37fa95ee8e8899d265a0f50fe4f52e898f
Shuhei Maruyama <shuheimaruyama@ShuheinoMacBook-Pro.local>
2019-10-01 16:24:25 +0900

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_115323) do
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider", null: false
     t.string "uid", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -96,29 +96,6 @@ ActiveRecord::Schema.define(version: 2019_09_28_115323) do
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "username", null: false
-    t.text "user_description"
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_name_in_katakana", null: false
-    t.string "first_name_in_katakana", null: false
-    t.integer "birth_year", null: false
-    t.integer "birth_month", null: false
-    t.integer "birth_day", null: false
-    t.string "phone_number", null: false
-    t.bigint "address_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
