f4cd72e3d98c656ad5ccdf07e528bc3dbf803a43
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-10-01 16:20:23 +0900

    card_purchase

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,25 +1,65 @@
 class PurchaseController < ApplicationController
+    require 'payjp'
+    before_action :set_good
     def index
-
+      card = Card.where(user_id: current_user.id).first
+      if card.blank?
+        redirect_to controller: "cards", action: "new"
+      else
+        Payjp.api_key = Rails.application.credentials.payjp[:secret]
+        customer = Payjp::Customer.retrieve(card.customer_id)
+        @default_card_information = customer.cards.retrieve(card.card_id)
+      end
     end
 
-# 以下テスト用ダミーです。
+    def pay
+      card = Card.where(user_id: current_user.id).first
+        Payjp.api_key = Rails.application.credentials.payjp[:secret]
+        Payjp::Charge.create(
+        :amount => @good.price, 
+        :customer => card.customer_id,
+        :currency => 'jpy', 
+      )
+      redirect_to action: 'done' #完了画面に移動
+    end
+
+    def done
+    end
+
+    def 
     def show
-        # @image = Image.all
-        # @image = Image.find(17)
-        # @good = Good.find(1)
-        @good = Good.find(params[:id])
     end
+
+    def select
+      # binding.pry
+      card = Card.where(user_id: current_user.id).first
+      if card.blank?
+        redirect_to controller: "cards", action: "new"
+      else
+        Payjp.api_key = Rails.application.credentials.payjp[:secret]
+        customer = Payjp::Customer.retrieve(card.customer_id)
+        @default_card_information = customer.cards.retrieve(card.card_id)
+      end
+
+      
+    end
+
+private
+  def set_good
+    # binding.pry
+    @good = Good.find(params[:id])
+  end
+
 
     # payjpの支払い記述(神山)
-    def pay
-        @good = Good.find(params[:id])
-        Payjp.api_key = 'sk_test_2ef78e8e70f19787263e2df5'
-        charge = Payjp::Charge.create(
-        :amount => @good.price,
-        :card => params['payjp-token'],
-        :currency => 'jpy',
-    )
-    end
+    # def pay
+    #     @good = Good.find(params[:id])
+    #     Payjp.api_key = 'sk_test_2ef78e8e70f19787263e2df5'
+    #     charge = Payjp::Charge.create(
+    #     :amount => @good.price,
+    #     :card => params['payjp-token'],
+    #     :currency => 'jpy',
+    # )
+    # end
 
 end 
