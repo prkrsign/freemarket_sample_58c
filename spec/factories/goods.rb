7b560ab9d6114ca3877bdf4942d03be3550e5743
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-10-02 21:48:59 +0900

    commit

--- spec/factories/goods.rb
+++ spec/factories/goods.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :good do
-    
+    goods_name                    {"Tシャツ"}
+    goods_description             {"ぜひご検討ください。"}
+    category_id                   {1}
+    size                          {"FREE SIZE"}
+    brand_id                      {1}
+    condition_id                  {1}
+    delivery_id                   {1}
+    prefecture_id                 {1}
+    shipment_id                   {1}
+    price                         {300}
+    user_id                       {1}
   end
 end
