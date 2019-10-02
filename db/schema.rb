f9cb79f7ab23936cebf10fe1a8eee6d469b36304
Shuhei Maruyama <shuheimaruyama@ShuheinoMacBook-Pro.local>
2019-10-02 10:52:30 +0900

    add NT to user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_232503) do
+ActiveRecord::Schema.define(version: 2019_10_02_014424) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
